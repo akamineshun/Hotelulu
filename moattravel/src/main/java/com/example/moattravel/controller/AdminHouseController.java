cb9f8beb39bb2af5b4bfe755aa3a06acffd08c7a
220291p <220291p@ugs.kochi-tech.ac.jp>
2025-04-09 17:37:49 +0900

    8.3 ページリンクを作成しよう

--- moattravel/src/main/java/com/example/moattravel/controller/AdminHouseController.java
+++ moattravel/src/main/java/com/example/moattravel/controller/AdminHouseController.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,7 +25,7 @@ public class AdminHouseController {
 	}
 	
 	@GetMapping
-	public String index(Model model, Pageable pageable) {
+	public String index(Model model, @PageableDefault(page = 0, size=10, sort="id", direction= Direction.ASC) Pageable pageable) {
 		List<House> houses = houseRepository.findAll();
 		Page<House> housePage = houseRepository.findAll(pageable);
 		
